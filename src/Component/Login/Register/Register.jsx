5d5928fec356aa7763061d893a18b052721c23f2
SadikRa <sadikrahman494@gmail.com>
2023-04-15 14:37:24 +0600

    register and login part done

--- src/Component/Login/Register/Register.jsx
+++ src/Component/Login/Register/Register.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import app from "../../../firebase/firebase.config";
+import { Link } from "react-router-dom";
 const auth = getAuth(app);
 const Register = () => {
     const [error, setError] = useState('');
@@ -13,6 +14,7 @@ const Register = () => {
     setSuccess('')
     const email = event.target.email.value;
         const password = event.target.password.value;
+        const name = event.target.name.value;
 
 
         if(!/^(?=.*[a-z])/.test(password)){
@@ -27,6 +29,7 @@ const Register = () => {
         setError('')
         event.target.reset()
         setSuccess('user login successfully')
+        updateUserData(result.user, name);
     })
     .catch(error => {
         console.error(error.message);
@@ -34,6 +37,18 @@ const Register = () => {
         setSuccess('')
     })
   };
+
+  const updateUserData = (user, name) => {
+    updateProfile(user, {
+        displayName: name
+    })
+        .then(() => {
+            console.log('user name updated')
+        })
+        .catch(error => {
+            setError(error.message);
+        })
+}
 
   const handleEmailChange = (event) => {
     // setEmail(event.target.value);
@@ -45,6 +60,8 @@ const Register = () => {
     <div className="w-50 mx-auto">
       <h4>Please Register</h4>
       <form onSubmit={handleSubmit}>
+      <input className='w-50 mb-4 rounded ps-2' type="text" name="name" id="name" placeholder='Your Name' required />
+                <br />
         <input
           type="email"
           name="email"
@@ -66,6 +83,7 @@ const Register = () => {
         <br />
         <input onClick={handlePasswordBlur} type="submit" value="register" />
       </form>
+      <p>already have register please <Link to={'/login'}>Login</Link></p>
       <p className="text-danger">{error}</p>
       <p className="text-primary">{success}</p>
     </div>
